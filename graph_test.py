eeec1287574d0373f979219746ca2fdab4d90b55
Roastedtoast029 <rockfield2357@gmail.com>
2023-04-20 13:42:23 +0900

    [add] 色、凡例、軸の体裁

--- graph_test.py
+++ graph_test.py
@@ -5,13 +5,21 @@ test_data = pd.read_csv("test_data.csv",index_col=0)
 
 df = test_data.apply(lambda x:x/x.sum()*100,axis=1)[::-1]
 
-print(df)
+#カラーマップを利用
+color = plt.get_cmap("Blues")
+
 fig, ax = plt.subplots()
-ax.barh(df.iloc[:,0].index, df.iloc[:,0],label=df.columns[0])
+ax.barh(df.iloc[:,0].index, df.iloc[:,0],label=df.columns[0],color=color(255//len(df)))
 stack=df.iloc[:,0]
 for column in range(1,len(df)):
-    ax.barh(df.index, df.iloc[:,column],left=stack, label=df.columns[column])
+    ax.barh(df.index, df.iloc[:,column],left=stack, label=df.columns[column],color=color(255//len(df)*(column+1)))
     stack += df.iloc[:,column]
 
-plt.legend()
+#もろもろの設定
+fig.legend(bbox_to_anchor=(1.1, 0.9),loc="upper right")
+ax.set_xlim(0,100)
+ax.set_xlabel("Peak Area Percentage (%)")
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
 plt.show()
